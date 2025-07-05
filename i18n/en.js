bdc0fbd5bf233b2c2970ac1f33d169a07eca52a2
Digital Alchemist <617020+miztizm@users.noreply.github.com>
2025-07-05 23:43:51 +0200

    Add files via upload

---
+++ i18n/en.js
@@ -0,0 +1,38 @@
+export default {
+  "current_language": "Current Language",
+  "language_switcher": "Language Switcher",
+  "language_changed": "Language changed successfully",
+  "language_change_error": "Failed to change language",
+    landing:{
+        "title": "the social brain network",
+        "intro": "In a world where digital templates rule and societal norms constrain, This is a grand journey into the boundless realms of what you can truly be. Forget the usual startup spiel; we're a living, breathing dynamic fusion of all things extraordinary. We see beyond the code – here's our message to the artist you are, the thoughts you ponder, and the dreams you chase. Here, you're not merely an end-user; you're a visionary. As you step into this electrifying sphere, you're instantly woven into an ever-evolving network, a hive mind of possibilities.",
+        "cta_text": "I'm interested",
+    },
+    header:{
+      "about": "About us",
+      "documentation": "Documentation",
+  },
+  about:{
+    "network": "Network",
+    "network_text_1": "Welcome to the autonomous virtual community, designed from the ground up to thrive on the collective human experience and interests. Here, your passion and engagement are not just welcomed—they're the very currency that powers and shapes our community's influence and infrastructure.",
+    "network_text_2": "Dive into a fully self-sustaining ecosystem where your contributions directly impact your growth, influence, and the broader community's future. This isn't just participation; it's a journey towards earning recognition, authority, and reward, all fueled by the synergy of individual efforts and digital collaboration.",
+    "network_text_3": "Join us and discover a world where passion and commitment are rewarded, where ideas and opinions are valued, and where the community is shaped by those who make it up. Together, we're stronger, smarter and more powerful than ever.",
+    "schema": "Schema",
+    "2030_text_1": "2030 marks the dawn of schema, a pivotal moment in history where the collective strength of individuals across the globe converges to forge the most expansive neo organization ever envisioned. This is not just an community; it's a revolution that taps into the very essence of our individual quantitative and qualitative attributes.",
+    "2030_text_2": "We're on the cusp of something extraordinary—a community-driven powered by missions that matter, cryptocurrency that empowers, and lifestyles that redefine the status quo. Imagine a place where hobby clubs ignite passions, festivals celebrate our unity in diversity, and media artists craft realities yet unseen.",
+    "2030_text_3": "This is a call to the neo-startups, the visionary coders, and every single soul yearning for a slice of the future, today. Together, we are crafting a new paradigm, a testament to what we can achieve when we harness our collective ambitions  Where every contribution shapes our journey, where every idea propels us forward, and where every individual is a cornerstone of our collective dream.",
+    "2030_text_4": "Join us on this exhilarating journey to 2030. Be a part of this monumental shift where your passions fuel our collective pursuit of innovation, community, and transformation. Together, we are not just dreaming of a new tomorrow; we are creating it.",
+},
+    auth:{
+      "signin": "Sign in",
+      "signup": "Sign up",
+      "signout": "Sign out",
+      "schema_network": "Schema Network",
+      "login_with_social_media": "Login with social media or your credentials",
+      "email_address": "Email address",
+      "password": "Password",
+      "remember_my_device": "Remember my device",
+      "forgot_password": "Forgot your password?",
+      "connecting": "Connecting...",
+  }
+}
---
+++ i18n/fr.js
@@ -0,0 +1,38 @@
+export default {
+  "current_language": "Langue actuelle",
+  "language_switcher": "Sélecteur de langue",
+  "language_changed": "Langue changée avec succès",
+  "language_change_error": "Échec du changement de langue",
+  landing:{
+      "title": "le réseau cérébral social",
+      "intro": "Dans un monde où les modèles numériques dominent et où les normes sociétales sont contraignantes, Ceci est un grand voyage dans les domaines illimités de ce que vous pouvez vraiment être. Oubliez le discours habituel des startups ; nous sommes une fusion dynamique et vivante de tout ce qui est extraordinaire. Nous voyons au-delà du code - voici notre message à l'artiste que vous êtes, aux pensées que vous méditez et aux rêves que vous poursuivez. Ici, vous n'êtes pas simplement un utilisateur final, vous êtes un visionnaire. En pénétrant dans cette sphère électrisante, vous êtes instantanément tissé dans un réseau en constante évolution, un esprit de ruche plein de possibilités.",
+      "cta_text": "je suis intéressé",
+  },
+  header:{
+    "about": "À Propos",
+    "documentation": "Documentation",
+},
+about:{
+  "network": "Réseau",
+  "network_text_1": "Bienvenue dans la communauté virtuelle autonome, conçue dès le départ pour prospérer grâce à l'expérience et aux intérêts humains collectifs. Ici, votre passion et votre engagement ne sont pas seulement les bienvenus : ils sont la monnaie qui alimente et façonne l'influence et l'infrastructure de notre communauté.",
+  "network_text_2": "Plongez dans un écosystème entièrement autonome où vos contributions ont un impact direct sur votre croissance, votre influence et l'avenir de la communauté au sens large. Il ne s'agit pas d'une simple participation, mais d'un voyage vers la reconnaissance, l'autorité et la récompense, le tout alimenté par la synergie des efforts individuels et de la collaboration numérique.",
+  "network_text_3": "Rejoignez-nous et découvrez un monde où la passion et l'engagement sont récompensés, où les idées et les opinions sont valorisées et où la communauté est façonnée par ceux qui la composent. Ensemble, nous sommes plus forts, plus intelligents et plus puissants que jamais.",
+  "schema": "Schema",
+  "2030_text_1": "2030 marque l'avènement de schema, un moment charnière de l'histoire où la force collective des individus du monde entier converge pour forger la néo-organisation la plus étendue jamais envisagée. Il ne s'agit pas seulement d'une communauté, mais d'une révolution qui puise dans l'essence même de nos attributs quantitatifs et qualitatifs individuels.",
+  "2030_text_2": "Nous sommes à l'aube de quelque chose d'extraordinaire : une communauté animée par des missions qui comptent, des crypto-monnaies qui donnent du pouvoir et des modes de vie qui redéfinissent le statu quo. Imaginez un endroit où les clubs de loisirs déchaînent les passions, où les festivals célèbrent notre unité dans la diversité et où les artistes des médias créent des réalités encore invisibles.",
+  "2030_text_3": "C'est un appel aux néo-startups, aux codeurs visionnaires et à tous ceux qui aspirent à une part de l'avenir, aujourd'hui. Ensemble, nous créons un nouveau paradigme, un témoignage de ce que nous pouvons réaliser lorsque nous mobilisons nos ambitions collectives. Où chaque contribution façonne notre voyage, où chaque idée nous propulse vers l'avant, et où chaque individu est une pierre angulaire de notre rêve collectif.",
+  "2030_text_4": "Rejoignez-nous dans ce voyage exaltant vers 2030. Participez à ce changement monumental où vos passions alimentent notre quête collective d'innovation, de communauté et de transformation. Ensemble, nous ne nous contentons pas de rêver d'un nouveau lendemain, nous le créons.",
+},
+  auth:{
+    "signin": "Se connecter",
+    "signup": "S'inscrire",
+    "signout": "Se déconnecter",
+    "schema_network": "Reseau de Schema",
+    "login_with_social_media": "Connectez-vous avec les réseaux sociaux ou vos identifiants",
+    "email_address": "Adresse email",
+    "password": "Mot de passe",
+    "remember_my_device": "Se souvenir de mon appareil",
+    "forgot_password": "Vous avez oublié votre mot de passe ?",
+    "connecting": "Connexion...",
+}
+}
